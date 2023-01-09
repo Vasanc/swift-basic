a7c0dc7025e85f10c9fd41fb90827932ac99bb9f
Vasan <vasanc2003@gmail.com>
2023-01-09 20:41:33 +0530

    started class

--- class.playground/Contents.swift
+++ class.playground/Contents.swift
@@ -1,1 +1,15 @@
+class Person{
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int){
+        self.name = name
+        self.age = age
+    }
+    
+    func increaseAge(){
+        self.age+=1
+    }
+}
 
+let p = Person(name: "AA", age: 20)
